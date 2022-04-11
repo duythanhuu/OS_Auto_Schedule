ad5e1bcacdf5988dfcf11484fe75c2c5feb86ee5
duythanhuu <58507036+duythanhuu@users.noreply.github.com>
2022-04-11 16:14:00 +0700

    Add files via upload

--- OS_Auto_Schedule.py
+++ OS_Auto_Schedule.py
@@ -5,14 +5,21 @@ Lines = open("OS_Auto_Schedule.xml", "r").readlines()
 
 Line_Init_formats= []
 Line_Run_formats= []
+Line_others_formats = []
 
 for line in Lines:
     if "_Init" in line:
         Line_Init_formats.append(line.replace("E - core_analytics - missing process ", "<SW-SERVICE-REF>").strip() + "</SW-SERVICE-REF>\n")
-    if "_Run" in line:
+    elif "_Run" in line:
         Line_Run_formats.append(line.replace("E - core_analytics - missing process ", "<SW-SERVICE-REF>").strip() + "</SW-SERVICE-REF>\n")
+    else:
+        Line_others_formats.append(line.replace("E - core_analytics - missing process ", "<SW-SERVICE-REF>").strip() + "</SW-SERVICE-REF>\n")
 
 add_contents=[""]
+
+proc_others = "".join(Line_others_formats)
+add_contents.insert(0, proc_others)
+
 proc_runs = "".join(Line_Run_formats)
 add_contents.insert(0, proc_runs)
 
@@ -52,26 +59,33 @@ for line in contents:
     if (st_line==1):
         if "_Init</SW-SERVICE-REF>" in line:
             section.append(c)
-            print("proc init add line: ",c)
+            #print("proc init add line: ",c)
             st_line += 1
             
 
     if (st_line==3):
         if "_Run</SW-SERVICE-REF>" in line:
             section.append(c)
-            print("proc run add line: ",c)
+            #print("proc run add line: ",c)
             st_line = 0
         
         
-print(section)
+#print(section)
         
 proc_inits = "".join(Line_Init_formats)
-#print(proc_inits)
+print(proc_inits)
 contents.insert(section[1], proc_inits)
+print("Added from line: ",section[1], "\n***********************************************************\n")
 
 proc_runs = "".join(Line_Run_formats)
-#print(proc_runs)
+print(proc_runs)
 contents.insert(section[3]+1, proc_runs)
+print("Added from line: ",section[3]+1, "\n***********************************************************\n")
+
+proc_others = "".join(Line_others_formats)
+print(proc_others)
+contents.insert(section[3]+1, proc_runs)
+print("WARNING: Other procs please take care by hand")
 
 with open("Conf\\OS_Shell\\rba_osshell_sched_dy.xml", "w") as f:
     contents_str = "".join(contents)
