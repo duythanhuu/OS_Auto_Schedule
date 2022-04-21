ac96cd486496144753b234b96fba0c719c224a02
duythanhuu <58507036+duythanhuu@users.noreply.github.com>
2022-04-21 16:43:59 +0700

    Add files via upload

--- OS_Auto_Schedule.py
+++ OS_Auto_Schedule.py
@@ -1,5 +1,7 @@
 f1 = open("OS_Auto_Schedule.xml", "w").writelines([l for l in open("_tealeaves\\tealeaves.log").readlines() if  "E - core_analytics - missing process " in l])
 
+with open("Conf\\OS_Shell\\rba_osshell_sched_dy.xml", "r") as f:
+    contents = f.readlines()
 
 Lines = open("OS_Auto_Schedule.xml", "r").readlines()
 
@@ -8,12 +10,15 @@ Line_Run_formats= []
 Line_others_formats = []
 
 for line in Lines:
-    if "_Init" in line:
-        Line_Init_formats.append(line.replace("E - core_analytics - missing process ", "<SW-SERVICE-REF>").strip() + "</SW-SERVICE-REF>\n")
-    elif "_Run" in line:
-        Line_Run_formats.append(line.replace("E - core_analytics - missing process ", "<SW-SERVICE-REF>").strip() + "</SW-SERVICE-REF>\n")
-    else:
-        Line_others_formats.append(line.replace("E - core_analytics - missing process ", "<SW-SERVICE-REF>").strip() + "</SW-SERVICE-REF>\n")
+    str_add = line.replace("E - core_analytics - missing process ", "                    <SW-SERVICE-REF>").replace('\n','') + "</SW-SERVICE-REF>\n"
+
+    if str_add not in contents:
+        if "_Init" or "_SyncIni" or "_SyncAngular" in line:
+            Line_Init_formats.append(str_add)
+        elif "_Run" or "_SyncS0" or "_SyncS1" or "_Sync" in line:
+            Line_Run_formats.append(str_add)
+        else:
+            Line_others_formats.append(str_add)
 
 add_contents=[""]
 
@@ -31,19 +36,8 @@ with open("OS_Auto_Schedule.xml", "w") as f:
     f.write(add_contents_str)
 
 
-# C:\Users\THD2HC\Desktop\source\HD431\MG1CS051_H_RQONE029141670\Conf\OS_Shell\rba_osshell_sched_dy.xml
 
-with open("Conf\\OS_Shell\\rba_osshell_sched_dy.xml", "r") as f:
-    contents = f.readlines()
-line_Init_section = 0
-line_10ms_section = 0
-for line in contents:
-    if "<SHORT-NAME>OS_Ini_Task</SHORT-NAME>" in line:
-        line_Init_section = contents.index(line)
-        #print(line_Init_section)
-    if "<SHORT-NAME>OS_10ms_Task</SHORT-NAME>" in line:
-        line_10ms_section = contents.index(line)
-        #print(line_10ms_section)
+
 
 section=[]
 
@@ -57,35 +51,45 @@ for line in contents:
         st_line += 1
 
     if (st_line==1):
-        if "_Init</SW-SERVICE-REF>" in line:
+        if "_Init</SW-SERVICE-REF>" or "_SyncIni</SW-SERVICE-REF>" or "_SyncAngular</SW-SERVICE-REF>" in line:
             section.append(c)
             #print("proc init add line: ",c)
             st_line += 1
             
 
     if (st_line==3):
-        if "_Run</SW-SERVICE-REF>" in line:
+        if "_Run</SW-SERVICE-REF>" or "_SyncS0</SW-SERVICE-REF>" or "_SyncS1</SW-SERVICE-REF>" or "_Sync</SW-SERVICE-REF>" in line:
             section.append(c)
             #print("proc run add line: ",c)
             st_line = 0
         
         
-#print(section)
-        
-proc_inits = "".join(Line_Init_formats)
-print(proc_inits)
-contents.insert(section[1], proc_inits)
-print("Added from line: ",section[1], "\n***********************************************************\n")
+if Line_Init_formats == []:
+    print ('No line _Init added')
+else:
+    proc_inits = "".join(Line_Init_formats)
+    print(proc_inits)
+    contents.insert(section[1], proc_inits)
+    print("Added from line: ",section[1])
+print ("\n***********************************************************\n")
 
-proc_runs = "".join(Line_Run_formats)
-print(proc_runs)
-contents.insert(section[3]+1, proc_runs)
-print("Added from line: ",section[3]+1, "\n***********************************************************\n")
+if Line_Run_formats == []:
+    print ('No line _Run added')
+else:
+    proc_runs = "".join(Line_Run_formats)
+    print(proc_runs)
+    contents.insert(section[3]+1, proc_runs)
+    print("Added from line: ",section[3]+1)
+print("\n***********************************************************\n")
 
-proc_others = "".join(Line_others_formats)
-print(proc_others)
-contents.insert(section[3]+1, proc_runs)
-print("WARNING: Other procs please take care by hand")
+if Line_others_formats == []:
+    print ('No line _Other added')
+else:
+    proc_others = "".join(Line_others_formats)
+    print(proc_others)
+    #contents.insert(section[3]+1, proc_runs)
+    print("WARNING: Other procs please take care by hand")
+print("\n***********************************************************\n")
 
 with open("Conf\\OS_Shell\\rba_osshell_sched_dy.xml", "w") as f:
     contents_str = "".join(contents)
