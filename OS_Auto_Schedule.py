c200798f79db6df8855812f121ac8b2881811847
duythanhuu <58507036+duythanhuu@users.noreply.github.com>
2022-04-29 08:07:59 +0700

    Add files via upload

--- OS_Auto_Schedule.py
+++ OS_Auto_Schedule.py
@@ -11,16 +11,16 @@ Line_others_formats = []
 
 for line in Lines:
     str_add = line.replace("E - core_analytics - missing process ", "                    <SW-SERVICE-REF>").replace('\n','') + "</SW-SERVICE-REF>\n"
-
     if str_add not in contents:
-        if "_Init" or "_SyncIni" or "_SyncAngular" in line:
+        if ("_Init</SW-SERVICE-REF>" or "_SyncIni</SW-SERVICE-REF>" or "_SyncAngular</SW-SERVICE-REF>") in str_add:
             Line_Init_formats.append(str_add)
-        elif "_Run" or "_SyncS0" or "_SyncS1" or "_Sync" in line:
+        elif ("_Run</SW-SERVICE-REF>" or "_SyncS0</SW-SERVICE-REF>" or "_SyncS1</SW-SERVICE-REF>" or "_Sync</SW-SERVICE-REF>") in str_add:
             Line_Run_formats.append(str_add)
         else:
             Line_others_formats.append(str_add)
 
 add_contents=[""]
+
 
 proc_others = "".join(Line_others_formats)
 add_contents.insert(0, proc_others)
@@ -69,7 +69,7 @@ if Line_Init_formats == []:
 else:
     proc_inits = "".join(Line_Init_formats)
     print(proc_inits)
-    contents.insert(section[1], proc_inits)
+    contents.insert(section[1]+10, proc_inits)
     print("Added from line: ",section[1])
 print ("\n***********************************************************\n")
 
@@ -78,7 +78,7 @@ if Line_Run_formats == []:
 else:
     proc_runs = "".join(Line_Run_formats)
     print(proc_runs)
-    contents.insert(section[3]+1, proc_runs)
+    contents.insert(section[3]+11, proc_runs)
     print("Added from line: ",section[3]+1)
 print("\n***********************************************************\n")
 
